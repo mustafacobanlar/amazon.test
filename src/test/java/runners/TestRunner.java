21f90d575f2457538cb9531fd74be26135f56c76
mustafacobanlar <mcobanlar34@gmail.com>
2023-04-17 15:28:33 +0300

    remove "s" character

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -10,5 +10,5 @@ import org.junit.runner.RunWith;
         glue = "stepdefs",
         plugin = {"pretty", "html:target/cucumber-reports"}
 )
-public clsass TestRunner {
+public class TestRunner {
 }
